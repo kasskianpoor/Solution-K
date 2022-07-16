799676214448f8b6643864f66c6fbe3493d0b4a2
kkianpoo <kiarash_kianpoor@sfu.ca>
2022-07-16 10:22:22 -0700

    opening the modal with url and closing buttons work

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 /*!
-* Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Agency v7.0.10 (https://startbootstrap.com/theme/agency)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
     // Navbar shrink function
     // var navbarShrink = function () {
     //     const navbarCollapsible = document.body.querySelector('#mainNav');
@@ -23,32 +22,49 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // };
 
-    // Shrink the navbar 
-    navbarShrink();
+    // Shrink the navbar
+    // navbarShrink();
 
     // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+    // document.addEventListener("scroll", navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
+    const mainNav = document.body.querySelector("#mainNav");
     if (mainNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
+            target: "#mainNav",
             offset: 74,
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
+        document.querySelectorAll("#navbarResponsive .nav-link")
     );
     responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
                 navbarToggler.click();
             }
         });
     });
 
+    console.log(window.location.hash);
+    const efiModal = document.getElementById("portfolioModal1");
+    if (window.location.hash == "#portfolioModal1") {
+        efiModal.style.display = "block";
+        efiModal.classList.remove("fade");
+        console.log("window.location.hash");
+    }
+    const closeModal1 = document.getElementById("closeModal1");
+    closeModal1.addEventListener("click", () => {
+        efiModal.style.display = "none";
+        efiModal.classList.add("fade");
+    });
+    const closeModal1x = document.getElementById("closeModal1x");
+    closeModal1x.addEventListener("click", () => {
+        efiModal.style.display = "none";
+        efiModal.classList.add("fade");
+    });
 });
